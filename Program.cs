e187f7b1a07e756de192dd277e8ca9e4655a39a6
connersmartin <connersmartin@gmail.com>
2019-05-24 22:02:02 -0700

    Potentially figured out multiple kids... need someone to test it out

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace Tadpole
             Console.Clear();
 
             //starts the webpage
-            IWebDriver driver = new ChromeDriver(@"..\netcoreapp2.2");
+            IWebDriver driver = new ChromeDriver("C:/Users/Owner/Downloads/");
             driver.Url = "https://www.tadpoles.com/home_or_work";
             
             //Log in to site
@@ -32,42 +32,52 @@ namespace Tadpole
             //wait a little bit
             Thread.Sleep(1000);
 
-            //get the list of months
-            var monthList = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div[1]/ul"));
-            var months = monthList.FindElements(By.TagName("li"));
+            //get the list of kids
+            var kidList = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[2]/div[3]/ul"));
+            var kids = kidList.FindElements(By.TagName("li"));
 
-            foreach (var month in months)
+            foreach (var c in kids)
             {
-                //Select the given month
-                month.Click();
-                //wait for pics to load
-                Thread.Sleep(5000);
+                c.Click();
+                //wait a little bit
+                Thread.Sleep(1000);
+                //get the list of months
+                var monthList = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div[1]/ul"));
+                var months = monthList.FindElements(By.TagName("li"));
 
-                //get list of pics
-                var liList = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div[2]/ul"));
-                var items = liList.FindElements(By.TagName("li"));
-
-                foreach (var item in items)
+                foreach (var month in months)
                 {
-                   //click on the picture
-                    item.Click();
-                    //wait for it to load
-                    Thread.Sleep(3000);
-                    //look to see if the item is available to download
-                    var icon = driver.FindElements(By.Id("sharetray-map"));
-                    if (icon.Count>0)
-                    {
-                        //keep running total of pictures downloaded
-                        count++;
-                        //Click the download button
-                        driver.FindElement(By.CssSelector("map[id='sharetray-map'] area:nth-child(3)")).Click();
-                    }
-                    //This is here for a slideshow effect, can remove to make process faster
-                    Thread.Sleep(3000);
+                    //Select the given month
+                    month.Click();
+                    //wait for pics to load
+                    Thread.Sleep(5000);
 
-                    //close the pic
-                    driver.FindElement(By.ClassName("btnClose")).Click();
-                    Thread.Sleep(1000);
+                    //get list of pics
+                    var liList = driver.FindElement(By.XPath("//*[@id=\"app\"]/div[3]/div[2]/ul"));
+                    var items = liList.FindElements(By.TagName("li"));
+
+                    foreach (var item in items)
+                    {
+                       //click on the picture
+                        item.Click();
+                        //wait for it to load
+                        Thread.Sleep(3000);
+                        //look to see if the item is available to download
+                        var icon = driver.FindElements(By.Id("sharetray-map"));
+                        if (icon.Count>0)
+                        {
+                            //keep running total of pictures downloaded
+                            count++;
+                            //Click the download button
+                            driver.FindElement(By.CssSelector("map[id='sharetray-map'] area:nth-child(3)")).Click();
+                        }
+                        //This is here for a slideshow effect, can remove to make process faster
+                        Thread.Sleep(3000);
+
+                        //close the pic
+                        driver.FindElement(By.ClassName("btnClose")).Click();
+                        Thread.Sleep(1000);
+                    }
                 }
             }
 
